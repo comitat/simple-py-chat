28d64b52f2cae4262c260949ca838b1c6d6e5eef
Evgeny Sidorov <ev.sdrv@ya.ru>
2020-03-26 00:15:37 +0500

    add /status

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import time
 
 app = Flask(__name__)
 
@@ -9,8 +10,11 @@ def hello():
 
 @app.route("/status")
 def status():
+    current_time = time.asctime()
     return{
-        'status': True
+        'status': True,
+        'name': 'simple messenger',
+        'time': current_time
     }
 
 app.run()
